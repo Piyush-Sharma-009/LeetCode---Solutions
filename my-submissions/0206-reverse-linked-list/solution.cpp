bc011ca5cfe1176736fc8bec507b8ed860e2ab92
Piyush-Sharma-009 <piyushsh009@gmail.com>
2025-05-01 18:19:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 13.3 MB (93.36%)

--- my-submissions/0206-reverse-linked-list/solution.cpp
+++ my-submissions/0206-reverse-linked-list/solution.cpp
@@ -11,10 +11,16 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        if(head == NULL || head->next == NULL) return head;
-        ListNode* h2 = reverseList(head->next);
-        head->next->next = head;
-        head->next = NULL;
-        return h2;
+        if(head == NULL|| head->next == NULL)
+        return head;
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        while(curr){
+            head = head->next;
+            curr->next = prev;
+            prev = curr;
+            curr = head;
+        }
+        return prev;
     }
 };
