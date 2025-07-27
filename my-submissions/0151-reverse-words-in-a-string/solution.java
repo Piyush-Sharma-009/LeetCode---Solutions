ded9d3fbda7cf189f715b695d350c38e6ebd7f20
Piyush-Sharma-009 <piyushsh009@gmail.com>
2025-07-27 15:50:02 +0000

    Sync LeetCode submission Runtime - 3 ms (99.16%), Memory - 42.3 MB (100.00%)

---
+++ my-submissions/0151-reverse-words-in-a-string/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder rev = new StringBuilder();
+        int i = s.length()-1;
+        while(i>=0){
+            while(i>=0 && s.charAt(i)==' ')i--;
+            if(i<0) break;
+            int end = i;
+            while(i>=0 && s.charAt(i)!= ' ')i--;
+            int start = i+1;
+            for(int j = start;j<=end;j++){
+                rev.append(s.charAt(j));
+            }
+            rev.append(' ');
+        }
+        //removing the last space
+        if(rev.length()>0) {
+        rev.setLength(rev.length()-1);
+        }
+        return rev.toString();
+    }
+}
+
