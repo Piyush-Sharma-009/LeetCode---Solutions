fe00c871b5ab9389715f7bf1c2a45304b0b288d9
Piyush-Sharma-009 <piyushsh009@gmail.com>
2025-08-06 06:35:16 +0000

    Sync LeetCode submission Runtime - 3 ms (57.55%), Memory - 43.7 MB (99.08%)

--- my-submissions/0125-valid-palindrome/solution.java
+++ my-submissions/0125-valid-palindrome/solution.java
@@ -1,20 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
         StringBuilder str = new StringBuilder();
-        for(int i = 0;i<s.length();i++){
-            if(s.charAt(i)>='A' && s.charAt(i)<='Z'){
-                str.append((char)(s.charAt(i)+32));
+        for(int i =0;i<s.length();i++){
+            char ch  = s.charAt(i);
+             if(Character.isLetterOrDigit(ch))
+            {
+                if(ch >= 'A' && ch <= 'Z'){
+                    ch = (char)(ch + 32);
+                }
+            str.append(ch);
             }
-            else if(s.charAt(i)>='a' && s.charAt(i)<='z'){
-                str.append(s.charAt(i));
-            }
-            else if(s.charAt(i)>='0'&&s.charAt(i) <='9')
-            str.append(s.charAt(i));
+            else 
+            continue;
         }
-        String org = str.toString();
-        String rev = str.reverse().toString();
-
-        return org.equals(rev);
-
+        char chars[] = str.toString().toCharArray();
+        int left = 0;
+        int right = chars.length -1;
+        while(left < right){
+            if(chars[left] == chars[right]){
+                left ++;
+                right --;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
     }
 }
