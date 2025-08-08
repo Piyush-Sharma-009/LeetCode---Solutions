770009d8b47ebaf21233bb0f7b1bbaa8228e385f
Piyush-Sharma-009 <piyushsh009@gmail.com>
2025-08-08 17:17:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.9 MB (100.00%)

---
+++ my-submissions/0050-powx-n/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n<0){
+            x = 1/x;
+            n = -n;
+        }
+        return powHelper(x,n);
+    }
+    public double powHelper(double x,int n){
+        if(n == 0) return 1.0;
+
+        double half = powHelper(x,n/2);
+        if(n%2 == 0){
+            return half*half;
+        }
+        else{
+            return half*half*x;
+        }
+    }
+}
