8ea9380ef20dfd05bd88052f6ba21f699f777686
Piyush-Sharma-009 <piyushsh009@gmail.com>
2026-02-14 05:16:02 +0000

    Sync LeetCode submission Runtime - 4 ms (49.78%), Memory - 17.9 MB (63.99%)

---
+++ my-submissions/2469-longest-subsequence-with-limited-sum/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+        vector<int> ans;
+        sort(nums.begin(),nums.end());
+        for(int i = 0;i<queries.size();i++){
+            int max = 0;
+            int sum = 0;
+            for(int j = 0;j<nums.size();j++){
+                sum += nums[j];
+                if(sum <= queries[i]){
+                    max++;
+                }
+                else
+                break;
+            }
+            ans.push_back(max);
+        }
+        return ans;
+    }
+};
