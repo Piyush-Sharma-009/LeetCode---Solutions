f1035f091952909c67ccf2601f335494fd4600d8
Piyush-Sharma-009 <piyushsh009@gmail.com>
2026-04-22 04:48:20 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 14.1 MB (80.20%)

--- my-submissions/0153-find-minimum-in-rotated-sorted-array/solution.cpp
+++ my-submissions/0153-find-minimum-in-rotated-sorted-array/solution.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-        if(nums.size() == 1)
-        return nums[0];
-        if(nums.size() == 0)
-        return 0;
-        
-        int pivot = 0;
-        for(int i = 0;i<nums.size() -2;i++){
-            if(nums[i]>nums[i+1]){
-                pivot = i;
-                break;
+        int left = 0, right = nums.size() - 1;
+
+        while(left < right){
+            int mid = (left + right)/2;
+
+            if(nums[mid] > nums[right]){
+                left = mid + 1;
             }
-            pivot = i+1;
+            else{
+                right = mid;
+            }
         }
-       
-       return min(nums[0],nums[pivot+1]);
+        return nums[left];
     }
 };
+
