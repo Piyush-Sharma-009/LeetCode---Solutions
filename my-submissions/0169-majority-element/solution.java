a5a30a267b3d63699e2237acc7a211bc2c24d139
Piyush-Sharma-009 <piyushsh009@gmail.com>
2026-06-06 12:11:08 +0000

    Sync LeetCode submission Runtime - 16 ms (21.08%), Memory - 52.4 MB (95.12%)

---
+++ my-submissions/0169-majority-element/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        int maj = 0;
+        for(int num : nums){
+            map.put(num,map.getOrDefault(num,0) + 1);
+            if(map.get(num) > nums.length/2){
+                maj = num;
+                break;
+            }
+        }
+        return maj;
+
+    }
+}
