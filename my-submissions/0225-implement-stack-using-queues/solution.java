4d9971c96495b227d74ae9eca0c9446a168814b0
Piyush-Sharma-009 <piyushsh009@gmail.com>
2026-06-07 13:28:08 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.9 MB (53.48%)

---
+++ my-submissions/0225-implement-stack-using-queues/solution.java
@@ -0,0 +1,44 @@
+class MyStack {
+    Queue<Integer> input;
+    Queue<Integer> output;
+    
+
+    public MyStack() {
+        input = new LinkedList<>();
+        output = new LinkedList<>();
+    }
+    
+    public void push(int x) {
+        output.offer(x);
+
+        while(!input.isEmpty()){
+            output.offer(input.poll());
+        }
+
+        Queue<Integer> temp = input;
+        input = output;
+        output = temp;
+
+    }
+    
+    public int pop() {
+        return input.poll();
+    }
+    
+    public int top() {
+        return input.peek();
+    }
+    
+    public boolean empty() {
+        return input.isEmpty();
+    }
+}
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack obj = new MyStack();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.top();
+ * boolean param_4 = obj.empty();
+ */
