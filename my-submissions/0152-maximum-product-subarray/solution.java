b3b2364c6002bc2dedde35e89339554769956028
Piyush-Sharma-009 <piyushsh009@gmail.com>
2026-06-24 17:40:32 +0000

    Sync LeetCode submission Runtime - 1 ms (92.99%), Memory - 47.4 MB (15.78%)

---
+++ my-submissions/0152-maximum-product-subarray/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+         int n = nums.length;
+        int maxp = Integer.MIN_VALUE;
+
+        int prefix = 1;
+        int suffix = 1;
+        for(int i = 0;i<n;i++){
+            
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+
+            prefix *= nums[i];
+            suffix *= nums[n-i-1];
+
+            maxp = Math.max(maxp,Math.max(prefix,suffix));
+        }
+        return maxp;
+    }
+}
