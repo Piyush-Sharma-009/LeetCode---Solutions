4a6a9db7f1c117464105df148f8bc292123ec59a
Piyush-Sharma-009 <piyushsh009@gmail.com>
2026-07-02 14:57:44 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 48.7 MB (71.63%)

--- my-submissions/0004-median-of-two-sorted-arrays/solution.java
+++ my-submissions/0004-median-of-two-sorted-arrays/solution.java
@@ -1,15 +1,27 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-     List<Integer> arr = new ArrayList<>();
-     int i = 0, j = 0;
-     while(i < nums1.length && j < nums2.length){
-        if(nums1[i] < nums2[j]) arr.add(nums1[i++]);
-        else arr.add(nums2[j++]);
-     }
-     while(i<nums1.length) arr.add(nums1[i++]);
-     while(j < nums2.length) arr.add(nums2[j++]);
-
-     if(arr.size() %2 == 1) return arr.get(arr.size()/2);
-     return (double) ((double) (arr.get(arr.size()/2)) + (double)(arr.get(arr.size()/2 - 1)))/2.0;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if(n1 > n2) return findMedianSortedArrays(nums2,nums1);
+        int low = 0, high = n1;
+        int left = (n1 + n2+ 1)/2;
+        int n = n1 + n2;
+        while(low <= high) {
+            int mid1 = (low + high) >> 1;
+            int mid2 = left - mid1;
+            int l1 = Integer.MIN_VALUE, l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE, r2 = Integer.MAX_VALUE;
+            if(mid1 < n1) r1 = nums1[mid1];
+            if(mid2 < n2) r2 = nums2[mid2];
+            if(mid1 - 1 >= 0) l1 = nums1[mid1-1];
+            if(mid2 - 1 >= 0) l2 = nums2[mid2-1];
+            if(l1 <= r2 && l2 <= r1) {
+                if(n %2 == 1) return Math.max(l1, l2);
+                return ((double)(Math.max(l1, l2) + Math.min(r1, r2))) / 2.0;
+            }
+            else if(l1 > r2) high = mid1 - 1;
+            else low = mid1 + 1;
+        }
+        return 0;
     }
 }
