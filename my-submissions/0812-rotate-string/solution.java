398d77368a3bab7680fb6fadb188b8fbbf5fd211
Piyush-Sharma-009 <piyushsh009@gmail.com>
2026-07-09 16:53:05 +0000

    Sync LeetCode submission Runtime - 2 ms (65.32%), Memory - 43.4 MB (67.79%)

--- my-submissions/0812-rotate-string/solution.java
+++ my-submissions/0812-rotate-string/solution.java
@@ -1,20 +1,9 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length() != goal.length())
-        return false;
-        if(s.equals(goal))
-        return true;
-        char[] chars = s.toCharArray();
-        int TotalRotate = s.length()-1;
-        for(int i = 0;i<=TotalRotate;i++){
-            char var = chars[0];
-            for(int j = 1;j<s.length();j++){
-                chars[j-1] = chars[j];
-            }
-            chars[s.length()-1] = var;
-            if(new String(chars).equals(goal))
-                return true;
-        }
-        return false;
+        if(s.length() != goal.length()) return false;
+        String doubled = s + s;
+
+        return doubled.contains(goal);
+
     }
 }
