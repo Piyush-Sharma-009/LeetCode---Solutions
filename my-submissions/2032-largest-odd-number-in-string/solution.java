b5e3cd94ebf6c907387a40c6288716ef21bb7c67
Piyush-Sharma-009 <piyushsh009@gmail.com>
2026-07-18 16:58:08 +0000

    Sync LeetCode submission Runtime - 1 ms (99.89%), Memory - 47 MB (56.23%)

--- my-submissions/2032-largest-odd-number-in-string/solution.java
+++ my-submissions/2032-largest-odd-number-in-string/solution.java
@@ -1,26 +1,9 @@
 class Solution {
     public String largestOddNumber(String num) {
-        int[] arr = new int[num.length()];
-        StringBuilder str = new StringBuilder();
-        for(int i = 0;i<num.length();i++){
-            arr[i] = num.charAt(i) - '0';
+        for(int i = num.length() - 1;i>=0;i--){
+            int digit = num.charAt(i) - '0';
+            if(digit %2 != 0) return num.substring(0,i+1);
         }
-        int start = 0;
-        int j = 0;
-        int end = 0;
-        int odd = 0;
-        while(j<num.length()){
-            if(arr[j]%2 != 0){
-                end = j;
-                odd++;
-            }
-                j++;
-        }
-            if(odd!= 0){
-            for(int i = start;i<=end;i++){
-                str.append(arr[i]);
-            }
-            }
-            return str.toString();
+        return "";
     }
 }
