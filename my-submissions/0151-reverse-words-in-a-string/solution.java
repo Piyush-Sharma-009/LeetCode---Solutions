22f308f8bf925e8299b4c161e2403d5ad86c5c3b
Piyush-Sharma-009 <piyushsh009@gmail.com>
2026-07-18 09:22:50 +0000

    Sync LeetCode submission Runtime - 4 ms (93.08%), Memory - 43.5 MB (99.17%)

--- my-submissions/0151-reverse-words-in-a-string/solution.java
+++ my-submissions/0151-reverse-words-in-a-string/solution.java
@@ -1,24 +1,51 @@
-
 class Solution {
-    public String reverseWords(String s) {
-        StringBuilder rev = new StringBuilder();
-        int i = s.length()-1;
-        while(i>=0){
-            while(i>=0 && s.charAt(i)==' ')i--;
-            if(i<0) break;
-            int end = i;
-            while(i>=0 && s.charAt(i)!= ' ')i--;
-            int start = i+1;
-            for(int j = start;j<=end;j++){
-                rev.append(s.charAt(j));
+    static void reverse(char[] a,int start, int n){
+        int i =start,j = n-1;
+        while(i<j){
+            char temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+            i++;
+            j--;
+        }
+        return;
+    }
+    static void reverseWord(char[] a, int n){
+        int i =0,j = 0;
+        while(i<n){
+            while(i<n && a[i] ==  ' '){
+                i++;
             }
-            rev.append(' ');
+            if(i>=n) break;
+            j = i;
+            while(j<n && a[j] != ' '){
+                j++;
+            }
+            reverse(a,i,j);
+            i = j;
         }
-        //removing the last space
-        if(rev.length()>0) {
-        rev.setLength(rev.length()-1);
+        return;
+    }
+    static String cleanSpaces(char[] a, int n){
+        int i =0,j =0;
+        while(j<n){
+            while(j<n && a[j] == ' '){
+                j++;
+            }
+
+            while(j <n && a[j] != ' '){
+                a[i++] = a[j++];
+            }
+            while(j<n && a[j] == ' ')j++;
+            if(j<n) a[i++] = ' ';
         }
-        return rev.toString();
+        return new String(a,0,i);
+    }
+    public String reverseWords(String s) {
+        int n = s.length();
+        char[] a = s.toCharArray();
+        reverse(a,0,n);
+        reverseWord(a,n);
+        return  cleanSpaces(a,n);
     }
 }
-
