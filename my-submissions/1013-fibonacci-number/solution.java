35ac84c6ff981f2650f1ab397fe69815b2183efe
Piyush-Sharma-009 <piyushsh009@gmail.com>
2026-08-04 19:30:39 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 41.9 MB (85.63%)

--- my-submissions/1013-fibonacci-number/solution.java
+++ my-submissions/1013-fibonacci-number/solution.java
@@ -1,6 +1,15 @@
 class Solution {
     public int fib(int n) {
-        if(n == 0 || n == 1)return n;
-        return fib(n-1) + fib(n-2);
+
+        if(n <= 1) return n;
+        int curri = 0;
+        int prev1  = 1;
+        int prev2 = 0;
+        for(int i = 2; i <=n;i++){
+            curri = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curri;
+        }
+        return curri;
     }
 }
